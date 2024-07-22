bd41f83923fdc5d62be574d32f36e6327e4b43f5
최태양 <choitaeyang0101@gmail.com>
2024-07-22 17:57:15 +0900

    feat: post add

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1721622582',resources: [
+    cacheName: 'chirpy-1721638624',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
