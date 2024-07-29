8dd9382e240f5d9deb26ae03db3aada5e04f6ffb
최태양 <choitaeyang0101@gmail.com>
2024-07-29 16:33:23 +0900

    feat:ollama post upload

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1722175876',resources: [
+    cacheName: 'chirpy-1722238323',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
